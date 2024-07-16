b6652751020fa65333f36d854a1e2dd0125ac0cd
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-16 15:02:34 +0200

    feat metiendo primer juego

---
+++ main.js
@@ -0,0 +1,12 @@
+import './main.scss'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const games = document.querySelectorAll('.game')
+
+  games.forEach((game) => {
+    game.addEventListener('click', () => {
+      const url = game.getAttribute('data-url')
+      window.location.href = url
+    })
+  })
+})
