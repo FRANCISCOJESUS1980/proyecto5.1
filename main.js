d57ee1b584f73402f2683d83466e1c940e177c91
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-24 16:47:30 +0200

    feat metiendo musica por fin

--- main.js
+++ main.js
@@ -3,10 +3,40 @@ import './main.scss'
 document.addEventListener('DOMContentLoaded', () => {
   const games = document.querySelectorAll('.game')
 
+  const musicUrl =
+    'https://www.bensound.com/bensound-music/bensound-relaxing.mp3'
+
+  const audio = new Audio(musicUrl)
+  audio.loop = true
+  audio.volume = 0.5
+  const isMusicPlaying = localStorage.getItem('isMusicPlaying') === 'true'
+
+  if (isMusicPlaying) {
+    audio.play().catch((error) => {
+      console.error('Error al reproducir la música:', error)
+    })
+  }
+
+  const startMusic = () => {
+    audio.play().catch((error) => {
+      console.error('Error al reproducir la música:', error)
+    })
+    document.removeEventListener('click', startMusic)
+  }
+
+  document.addEventListener('click', startMusic)
+
   games.forEach((game) => {
-    game.addEventListener('click', () => {
+    game.addEventListener('click', (event) => {
+      event.preventDefault()
       const url = game.getAttribute('data-url')
+
+      localStorage.setItem('isMusicPlaying', !audio.paused)
+
       window.location.href = url
     })
+  })
+  window.addEventListener('beforeunload', () => {
+    localStorage.setItem('isMusicPlaying', !audio.paused)
   })
 })
